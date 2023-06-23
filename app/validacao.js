9a7a4da58e7e96ba93f5cbf808d41ce06c8920fc
douglas <douglas.olliveira1997@gmail.com>
2023-06-23 19:06:04 -0300

    Incluindo a Pagina GAME OVER

--- app/validacao.js
+++ app/validacao.js
@@ -2,9 +2,21 @@ function verificaSeOchuteEumValorValido(chute) {
     const numero = + chute
 
     if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER") {
 
-        elementoChute.innerHTML += '<div> Valor invalido Tente Novamente </div>'
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-novamente" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
     }
+
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
         elementoChute.innerHTML +=`<div>valor inválido: Fale um numero entre ${menorValor} e ${maiorValor} </div>`
     }
@@ -15,11 +27,7 @@ function verificaSeOchuteEumValorValido(chute) {
         <button id= "jogar-novamente" class="btn-novamente">Jogar Novamente</button>
         `
 
-    }if("game over"== "game over"){
-        document.body.innerHTML = ` <h4>Fim de Jogo</h4>
-        <button id= "jogar-novamente" class="btn-novamente">Jogar Novamente</button>`
-    }
-    else if (numero > numeroSecreto){
+    }else if (numero > numeroSecreto){
          elementoChute.innerHTML += `<div>O número secreto e menor <i class="fa-solid fa-arrow-down-long"></i></div>`
     }
     else{
